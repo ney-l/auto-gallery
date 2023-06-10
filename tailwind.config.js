eb221e0e9c7c2ddac834691451bccacab3e1838b
Neha Demirci <neylanjewar@gmail.com>
2023-06-10 04:55:48 +0200

    updates font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
+        manrope: ['Manrope', 'sans-serif'],
       },
       colors: {
         'black-100': '#2B2C35',
