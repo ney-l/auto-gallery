9f92bc3aef55b9036c8d74688606aa6381371f37
Neha Demirci <neylanjewar@gmail.com>
2023-06-10 04:55:35 +0200

    updates hero section's bg image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,13 +29,12 @@ const Hero = () => {
             className="object-contain"
           />
           <div className="hero__image-overlay">
-            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fill="#0F62FE"
-                d="M51.8,-51.2C61.9,-41.8,61.2,-20.9,58.5,-2.7C55.8,15.6,51.3,31.1,41.2,45.7C31.1,60.2,15.6,73.7,0.3,73.4C-14.9,73,-29.7,58.8,-45.7,44.3C-61.7,29.7,-78.7,14.9,-79.2,-0.4C-79.6,-15.8,-63.4,-31.5,-47.5,-40.9C-31.5,-50.4,-15.8,-53.4,2.6,-56C20.9,-58.6,41.8,-60.6,51.8,-51.2Z"
-                transform="translate(100 100)"
-              />
-            </svg>
+            <Image
+              src="/hero-bg.svg"
+              alt="hero background"
+              fill
+              className="object-contain"
+            />
           </div>
         </div>
       </div>
